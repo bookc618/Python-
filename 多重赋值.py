74d84b239aca5f1a96f0f0a4c311bbdfa47fccf2
bookc618 <wangyi198818@hotmail.com>
2018-10-06 05:33:31 +0800

    多重赋值、字典转列表

---
+++ 多重赋值.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#-*- coding:utf-8 -*-
+# 
+
+spam = {'Red':'41','Green':'42'}
+for k,v in spam.items(): #多重赋值
+    print(k + "'s code is "+ v)
---
+++ 字典转列表.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#-*- coding:utf-8 -*-
+# 
+
+spam = {'Red':'41','Green':'42'}
+print(spam.keys())
+print(list(spam.keys())) #用list 方法转换字典，得到真正的list
