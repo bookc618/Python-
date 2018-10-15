9e4f769e4e16e838a4106ca02d0aab2db0c96809
bookc618 <wangyi198818@hotmail.com>
2018-10-15 16:15:49 +0800

    实现表格打印

--- 表格打印.py
+++ 表格打印.py
@@ -1,9 +1,36 @@
 #!/usr/bin/python3
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 #
+'''
+需求:
+    编写一个名为printTable()的函数，它接受字符串的列表的列表，将它显示在组
+    织良好的表格中，每列右对齐。假定所有内层列表都包含同样数目的字符串。
+'''
+
+import numpy as np
 
 
+def printtable(tabledata):
+    '''
+        此函数通过numpy函数以矩阵转置方式实现需求目标。
+    '''
+    tempdata = np.transpose(tabledata).tolist()
+    element = []
+    maxcolwidth = 0
+    for i in range(len(tempdata)):
+        for j in range(len(tempdata[i])):
+            element = tempdata[i][j]
+            if len(element) > maxcolwidth:
+                maxcolwidth = len(element)
 
-def printTable():
-    # TODO: 实际代码 https://blog.csdn.net/u012420395/article/details/80615488
-    pass
+    for i in range(len(tempdata)):
+        for j in range(len(tempdata[i])):
+            print(tempdata[i][j].rjust(maxcolwidth), end='')
+        print()
+
+
+if __name__ == '__main__':
+    tableData = [['apples', 'oranges', 'cherries', 'banana'],
+                 ['Alice', 'Bob', 'Carol', 'David'],
+                 ['dogs', 'cats', 'moose', 'goose']]
+    printtable(tableData)
